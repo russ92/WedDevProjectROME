5b829ada695ec1c750a5aa8aea78028b1266340f
OwenCorriganUCD <62837888+OwenCorriganUCD@users.noreply.github.com>
2022-03-15 19:30:17 +0000

    More Backend
    Working on sessions for login

--- src/main/java/app/UserAccount.java
+++ src/main/java/app/UserAccount.java
@@ -1,13 +1,18 @@
 package app;
 import java.io.Serializable;
 
+@Component
+@SessionScope
 public class UserAccount {
     private String username;
+    private String email;
     private String password;
 
     public String getUsername(){return username;}
+    public String getEmail(){return email;}
     public String getPassword(){return password;}
 
     public void setUsername(String username){this.username = username;}
+    public void setEmail(String email){this.email = email;}
     public void setPassword(String password){this.password = password;}
 }
